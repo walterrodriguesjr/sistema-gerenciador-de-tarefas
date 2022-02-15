4e1aa1dc481e80b1f7145103dd46a7b17f64fefd
walter <walterrjr.86@gmail.com>
2022-02-15 18:51:49 -0300

    atualizando

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Gerenciador de Tarefas</h1>
+        </div>
+        <div class="form">
+            <form action="" method="get">
+                <label for="task_name"></label>
+                <input type="text" name="task_name" placeholder="Nome da Tarefa:">
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
+        <div class="separator">
+        </div>
+        <div class="list-tasks">
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa 2</li>
+                <li>Tarefa 3</li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p>Desenvolvido por @Walter Rodrigues Junior</p>
+        </div>
+    </div>
+</body>
+
+</html>
