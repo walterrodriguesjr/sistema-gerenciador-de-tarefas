8a64eb891d77f1852515a6b867693faf9cead174
walter rodrigues junior <walterrjr.86@gmail.com>
2022-02-15 22:41:58 -0300

    atualizando

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['tasks'])) {
+    $_SESSION['tasks'] = array();
+}
+
+if (isset($_GET['task_name'])) {
+    array_push($_SESSION['tasks'], $_GET['task_name']);
+    unset($_GET['task_name']);
+}
+
+if (isset($_GET['clear'])) {
+    unset($_SESSION['tasks']);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -7,6 +26,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerenciador de Tarefas</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -16,7 +38,7 @@
         </div>
         <div class="form">
             <form action="" method="get">
-                <label for="task_name"></label>
+                <label for="task_name">Tarefa:</label>
                 <input type="text" name="task_name" placeholder="Nome da Tarefa:">
                 <button type="submit">Cadastrar</button>
             </form>
@@ -24,14 +46,25 @@
         <div class="separator">
         </div>
         <div class="list-tasks">
-            <ul>
-                <li>Tarefa 1</li>
-                <li>Tarefa 2</li>
-                <li>Tarefa 3</li>
-            </ul>
+            <?php
+
+if (isset($_SESSION['tasks'])) {
+    echo "<ul>";
+
+    foreach ($_SESSION['tasks'] as $key => $task) {
+        echo "<li>$task</li>";
+    }
+
+    echo "</ul>";
+}
+?>
+            <form action="" method="get">
+                <input type="hidden" name="clear" value="clear">
+                <button class="btn-clear" type="submit">Limpar Tarefas</button>
+            </form>
         </div>
         <div class="footer">
-            <p>Desenvolvido por @Walter Rodrigues Junior</p>
+            <p>Desenvolvido por @Walterrjr86</p>
         </div>
     </div>
 </body>
